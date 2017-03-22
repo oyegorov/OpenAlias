ddd11c81b901f5c29835799447cb57021601601b
Oleg Yegorov <yegorov.oleg@gmail.com>
2017-03-22 00:34:09 -0400

    Implemented teams page

--- OpenAlias/src/pages/teams/teams.ts
+++ OpenAlias/src/pages/teams/teams.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Teams page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
-  selector: 'page-teams',
-  templateUrl: 'teams.html'
+    selector: 'page-teams',
+    templateUrl: 'teams.html'
 })
 export class TeamsPage {
+    public selectedTeams = [];
+    private allTeamNames: string[] = ['Push Keen', 'Moo Ducks', 'Chromed Dogs', 'Suicide Squad', 'Climbing Monsters', 'Deam More Awes', 'Copy-Paste Ninjas', 'Jumping Monkeys', 'Ghostbusters'];
+    private availableTeams;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    constructor() {
+        this.availableTeams = this.allTeamNames.slice();
+        this.addTeam();
+        this.addTeam();
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TeamsPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad TeamsPage');
+    }
 
+    public addTeam() {
+        if (this.selectedTeams.length > 5)
+            return;
+
+        let teamIndex = Math.floor(Math.random() * this.availableTeams.length);
+
+        this.selectedTeams.push(this.availableTeams[teamIndex]);
+
+        var index = this.availableTeams.indexOf(this.availableTeams[teamIndex]);
+        this.availableTeams.splice(index, 1);
+    }
+
+    public deleteTeam(index: number) {
+        let teamToDelete = this.selectedTeams[index];
+        this.selectedTeams.splice(index, 1);
+
+        this.availableTeams.push(teamToDelete);
+    }
 }
