339745915a8030bac19817e30add4cdecb26ca13
Oleg Yegorov <yegorov.oleg@gmail.com>
2017-05-04 09:56:59 -0400

    Added rules localization possibility

--- OpenAlias/src/pages/rules/rules.ts
+++ OpenAlias/src/pages/rules/rules.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { LocalizationService } from "../../providers/localization-service";
 
 @Component({
     selector: 'page-rules',
     templateUrl: 'rules.html'
 })
 export class RulesPage {
-    constructor(public navCtrl: NavController, public navParams: NavParams) { }
+    constructor(public navCtrl: NavController, public navParams: NavParams, private localizationService: LocalizationService) { }
 }
